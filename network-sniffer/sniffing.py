a33c7bc0bf844ff67ecfd047eb02ab14b506b8e0
lincoln1006 <lphoffart@gmail.com>
2026-09-15 21:45:31 -0600

    added packet logging to sniffing.py

--- network-sniffer/sniffing.py
+++ network-sniffer/sniffing.py
@@ -1,4 +1,11 @@
 import socket
+import time
 s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-while True:
-    print(s.recvfrom(65565))
+starttime = time.time()
+numPackets = 0
+with open("packetlog.txt", "w") as f:
+	while True:
+		f.write(f"{s.recvfrom(65565)}\n")
+		curtime = time.time()
+		if round(curtime - starttime, 1) > 10:
+			exit()
